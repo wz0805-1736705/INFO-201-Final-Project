eb6dce542186d653a9c93592fa3423c4684383a4
grtyang <46581816+grtyang@users.noreply.github.com>
2019-03-08 16:02:13 -0800

    Update ui.R

--- Interactive Page #2 (Grant)/ui.R
+++ Interactive Page #2 (Grant)/ui.R
@@ -6,21 +6,21 @@ my.ui <- fluidPage(
   
   tabsetPanel(
     tabPanel(
-      titlePanel("ronney"),
-      h1("Analysis of the Average Rating Statistic"),
+      titlePanel("Sports distribution by countries"),
+      h1("Analysis of sports distribution by countries"),
       sidebarLayout(
         sidebarPanel(
-          selectInput("anime", "Choose the first anime", 
-                      choices = c("Cowboy Bebop", "Naruto", "Spirited Away", "Monster", "One Piece", "Trigun", "Berserk", "Fullmetal Alchemist", "Slam Dunk", "Dragon Ball", "Prince of Tennis", "Trinity Blood"),
-                      selected = "Naruto"),
-          selectInput("anime2", "Choose a second anime to compare to", 
+          selectInput("gender", "Choose a gender", 
+                      choices = c("Male", "Female"),
+                      selected = "Male"),
+          selectInput("sport", "Choose a sport", 
                       choices = c("Cowboy Bebop", "Naruto", "Spirited Away", "Monster", "One Piece", "Trigun", "Berserk", "Fullmetal Alchemist", "Slam Dunk", "Dragon Ball", "Prince of Tennis", "Trinity Blood"),
                       selected = "Cowboy Bebop"),
           h3("Background:"),
           p("In this interactive bar graph, there are dropdown menus that allow for the selection of TWO different anime series to compare between. On the title of the graph, the overall average user rating of the anime is listed for perspective comparison."),
           
           h3("Summary:"),
-          p("This graph is split up into 10 different sections, which represent what users rated the anime as (from 10% to 100%). When you hover over a specific bar, you will be able to see how many users voted for this specific rating. This gives us greater insight on where the Overall Rating Percentage comes from because we can see specifically what ratings contributed to the overall score.") 
+          p("") 
          ),
         
         mainPanel(
