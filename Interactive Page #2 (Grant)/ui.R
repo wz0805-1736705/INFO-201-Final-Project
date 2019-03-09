c4c6cb8e23bea1896ce01a3aa4ea7a680f7137aa
grtyang <46581816+grtyang@users.noreply.github.com>
2019-03-08 16:11:13 -0800

    Update ui.R

--- Interactive Page #2 (Grant)/ui.R
+++ Interactive Page #2 (Grant)/ui.R
@@ -14,8 +14,8 @@ my.ui <- fluidPage(
                       choices = c("Male", "Female"),
                       selected = "Male"),
           selectInput("sport", "Choose a sport", 
-                      choices = c("Cowboy Bebop", "Naruto", "Spirited Away", "Monster", "One Piece", "Trigun", "Berserk", "Fullmetal Alchemist", "Slam Dunk", "Dragon Ball", "Prince of Tennis", "Trinity Blood"),
-                      selected = "Cowboy Bebop"),
+                      choices = c("Athletics", "Badminton", "Basketball", "Boxing", "Cross country skiing", "Football", "Gymnastics", "Ice hockey", "Rowing", "Speed Skating", "Swimming", "Sailing"),
+                      selected = "Athletics"),
           h3("Background:"),
           p("In this interactive bar graph, there are dropdown menus that allow for the selection of TWO different anime series to compare between. On the title of the graph, the overall average user rating of the anime is listed for perspective comparison."),
           
