153092f20d79262c2815a07d4261c344e81b3125
姚安婕 <yaoanjie@D-10-19-134-81.dhcp4.washington.edu>
2019-03-10 17:44:14 -0700

    page2

--- Interactive Page #2 (Grant)/server.R
+++ Interactive Page #2 (Grant)/server.R
@@ -1,5 +1,47 @@
-library(dplyr)
-library(shiny)
-library(plotly)
-library(ggplot2)
+library("dplyr")
+library("ggplot2")
+library("shiny")
+library("styler")
+library("lintr")
+#style_file("ui.R")
+#lint("ui.R")
+
+shinyApp(ui = ui, server = server)
+dataset <- read.csv("data/athlete_events.csv", stringsAsFactors = F)
+dataset <- dataset%>%
+  filter(Year >= "1980")
+
+server <- function(input, output) {
+#CARRIE#
+filtered <- reactive({
+  filtered <- dataset %>%
+  filter(Sex == input$option,
+         Year == input$year, !is.na(Medal)) %>%
+  group_by(NOC, Medal)%>%
+  arrange(NOC) %>%
+  summarize(count = length(Medal))
+  
+  if(input$option == "Both"){
+    filtered <- dataset %>%
+      filter(Year == input$year, !is.na(Medal)) %>%
+      group_by(NOC, Medal)%>%
+      arrange(NOC) %>%
+      summarize(count = length(Medal))
+  }
+  return(filtered)
+})
+
+output$medalplot <- renderPlot({
+plot_m <- ggplot(filtered(), aes(x = NOC, y = count, fill = Medal)) +
+  geom_col(width = 0.8) +
+  coord_flip() + 
+  ggtitle(paste0("Medal Counts for ", input$option, 
+                 " at the ",input$year," Olympics")) +
+  scale_fill_manual(values = c("darkgoldenrod4", "gold1", "gray62") )
+  theme(plot.title = element_text(hjust = 0.5),
+       axis.text.y = element_text(size=6))
+plot_m
+#########
+})
+}
 
--- Interactive Page #2 (Grant)/ui.R
+++ Interactive Page #2 (Grant)/ui.R
@@ -1,13 +1,20 @@
-library('shiny')
-library('plotly')
+library("plotly")
+library("shiny")
+library("ggplot2")
+library("dplyr")
+library("styler")
+library("lintr")
+#style_file("ui.R")
+# lint("ui.R")
 
 
-my.ui <- fluidPage(
-  
-  tabsetPanel(
-    tabPanel(
-      titlePanel("Sports distribution by countries"),
-      h1("Analysis of sports distribution by countries"),
+ui <- fluidPage(
+navbarPage("Olympics Games Interactive Statistics",
+           
+###GRANT#### 
+    tabPanel("Sports distribution by countries",
+      titlePanel("Analysis of Sports Distribution by Countries at the Olympics"),
+      
       sidebarLayout(
         sidebarPanel(
           selectInput("gender", "Choose a gender", 
@@ -27,7 +34,42 @@ my.ui <- fluidPage(
           plotlyOutput("bargraph")
         )
       )
-    )  
+    ), 
+
+####CARRIE####
+    tabPanel("Medal Count of Gender at the Olympics",
+      titlePanel("Analysis of Medal Count of Gender at the Olympics"),
+        sidebarLayout(
+  sidebarPanel(
+                   selectInput("year",
+                               label = h4("Select the Games"),
+                               choices = c(
+                                 "1980", "1984", "1988", "1992", "1996",
+                                 "2000", "2004", "2008", "2012", "2016"
+                               ),
+                               selected = 1
+                   ),
+                   selectInput("option",
+                               label = h4("Select the gender"),
+                               choices = c("M", "F", "Both"),
+                               selected = 1
+                   ),
+                   helpText("This interactive allows you to select one
+                            Olympics Games to see statistics of medal 
+                            of female, male or both.")
+                   ),
+                 mainPanel(
+                   plotOutput("medalplot"),
+                   h3("Summary"),
+                   p("Each bar on this graph illustrates the medal count of
+                     each country in the specific year of the Olympics Games.
+                     We can see a growing number of countries had female athletes
+                     paticiapted in the Olympics and earned more medals in the game.
+                     It is also worth noticing that USA has the largest number of medals
+                     after 1996.")
+                   )
+                 )
+    )
+  ##########
   )
 )
-shinyUI(my.ui)
